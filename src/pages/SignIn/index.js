36958f251dfbdcc5e5ae527607e4766de34579f0
Sharlye Vieira de Melo Júnior <77028067+Sharlye@users.noreply.github.com>
2024-04-30 20:44:04 -0300

    cadastro de user

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,25 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import './signin.css'
 
 import logo from '../../assets/logo.png'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../../contexts/auth'
 
 
 export default function SignIn(){
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const { signIn, loadingAuth } = useContext(AuthContext)
+
+  async function handleSignIn(e){
+    e.preventDefault();
+
+    if(email !== '' && password !== ''){
+      await signIn(email, password);
+    }
+
+  }
 
 
   return(
@@ -17,7 +29,7 @@ export default function SignIn(){
           <img src={logo} alt="Logo do sistema de chamados" />
         </div>
 
-        <form>
+        <form onSubmit={handleSignIn}>
           <h1>Entrar</h1>
           <input 
             type="text" 
@@ -33,7 +45,9 @@ export default function SignIn(){
             onChange={ (e) => setPassword(e.target.value) }
           />
 
-          <button type="submit">Acessar</button>
+          <button type="submit">
+            {loadingAuth ? "Carregando..." : "Acessar"}
+          </button>
         </form>
 
         <Link to="/register">Criar uma conta</Link>
